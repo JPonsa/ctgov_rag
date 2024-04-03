6e44fb5ca097756e4b26eab51297d266c8110c1e
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-03 14:18:51 +0100

    increase timewall and power

--- hpc/run_embed_kg_node.sh
+++ hpc/run_embed_kg_node.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -l
 #$ -N ctGov_KG_Embedding
 # Max run time in H:M:S
-#$ -l h_rt=2:0:0
-#$ -pe smp 1
+#$ -l h_rt=6:0:0
 # Memory
-#$ -l mem=32G
+#$ -l mem=1G
+#$ -pe smp 2
+
 # workig directory. Use #S -cwd to use current working dir
 #$ -wd /home/rmhijpo/Scratch/ctgov_rag/
+module unload compilers mpi
+module load compilers/gnu/4.9.2
+module load cuda/7.5.18/gnu-4.9.2
 module load openssl/1.1.1t python/3.11.3
 pip install poetry
 # poetry config virtualenvs.in-project true
