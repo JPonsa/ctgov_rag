fc38cbfda14a53609fa7f9995f5cf6cdff1bf623
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-10 18:26:32 +0100

    ask for gpus and add resource usage tracking

--- hpc/run_embed_kg_node.sh
+++ hpc/run_embed_kg_node.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
 #$ -N ctGov_KG_Embedding
 # Max run time in H:M:S
-#$ -l h_rt=6:0:0
+#$ -l h_rt=10:0:0
 # Memory
 #$ -l mem=1G
-#$ -pe smp 2
+#$ -pe smp 1
+#$ -l gpu=2
 
 # workig directory. Use #S -cwd to use current working dir
 #$ -wd /home/rmhijpo/Scratch/ctgov_rag/
@@ -12,7 +13,9 @@ module unload compilers mpi
 module load compilers/gnu/4.9.2
 module load cuda/7.5.18/gnu-4.9.2
 module load openssl/1.1.1t python/3.11.3
+module load ruse/2.0
+
 pip install poetry
 # poetry config virtualenvs.in-project true
 # poetry install
-poetry run python embed_kg_nodes.py ./data/raw/knowledge_graph/ ./data/preprocessed/knowledge_graph/ 
+ruse --stdout --time=900 -s  poetry run python embed_kg_nodes.py ./data/raw/knowledge_graph/ ./data/preprocessed/knowledge_graph/ 
