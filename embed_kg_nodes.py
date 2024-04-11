2242beeeb96a7228461ff54442d0a899c53ebe83
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-11 21:12:29 +0100

    embed studies from MongoDB

--- embed_kg_nodes.py
+++ embed_kg_nodes.py
@@ -6,14 +6,12 @@ import numpy as np
 import pandas as pd
 import torch
 from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
 from trial2vec import Trial2Vec
 
 from src.embeddings.Trial2Vec_embedding import ct_dict2pd
-from src.utils.utils import get_clinical_trial_study, print_green, print_red
+from src.utils.utils import get_clinical_trial_study, print_green, print_red, connect_to_mongoDB
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-print(f"Using {DEVICE}")
 SEPARATOR = "|"
 PRECISION = 15
 # FORMATTER = {"float": lambda x: f"%.{PRECISION}f" % x}
@@ -36,14 +34,14 @@ def embedding_formatting(emb: np.array) -> str:
     return emb
 
 
-def trial2vect_encode_node(inp_path: str, out_path: str):
+def trial2vect_encode_node(input_path: str, output_path: str, args):
     """Apply trial2vec to the ClinicalTrail KG nodes
 
     Parameters
     ----------
-    inp_path : str
+    input_path : str
         input data path
-    out_path : str
+    output_path : str
         output data path
     """
 
@@ -51,22 +49,33 @@ def trial2vect_encode_node(inp_path: str, out_path: str):
     study_pd = pd.DataFrame()
 
     node_name = "ClinicalTrial"
-    header = pd.read_csv(f"{inp_path}{node_name}-header.csv", sep="\t")
-    df = pd.read_csv(f"{inp_path}{node_name}-part000.csv", sep="\t", header=None)
+    header = pd.read_csv(f"{input_path}{node_name}-header.csv", sep="\t")
+    df = pd.read_csv(f"{input_path}{node_name}-part000.csv", sep="\t", header=None)
     df.columns = header.columns
 
     df["trial2vec_emb:double[]"] = df["trial2vec_emb:double[]"].astype(object)
 
-    for i, nctId in enumerate(df[":ID"].values):
-        studies.append(get_clinical_trial_study(nctId))
+    if args.mongoDB: # Pull studies from mongoDB collection
+        with connect_to_mongoDB(user=args.user, pwd=args.pwd) as client:
+            db = client[args.db]
+            collection = db[args.collection]
+            print(f"Downloading studies from mongoDB db:{args.db} collection:{args.collection} ...")
+            studies = [doc for doc in collection.find({})]
+            
+    else: # Pull studies from ct.gov API
+        print("Downloading studies from ct.gov ...")
+        for nctId in df[":ID"].values:
+            studies.append(get_clinical_trial_study(nctId))
 
-    for i, study in enumerate(studies):
+    print("Reshaping studies to be embedded using trial2vec")
+    for study in studies:
         tmp = ct_dict2pd(study)
         study_pd = pd.concat([study_pd, tmp])
 
     embeddings = trial2vec.encode({"x": study_pd})
 
-    for i, row in tqdm(df.iterrows(), desc=f"Embedding {node_name}"):
+    print(f"Embedding {node_name}")
+    for i, row in df.iterrows():
 
         nctId = row[":ID"]
         emb = embeddings[nctId]
@@ -74,27 +83,27 @@ def trial2vect_encode_node(inp_path: str, out_path: str):
 
         df.at[i, "trial2vec_emb:double[]"] = emb_str
 
-    df.to_csv(f"{out_path}{node_name}-part000.csv", sep="\t", header=None, index=False)
+    df.to_csv(f"{output_path}{node_name}-part000.csv", sep="\t", header=None, index=False)
 
 
-def biobert_enconde_node(inp_path: str, out_path: str, node_name: str, term: str):
+def biobert_encode_node(input_path: str, output_path: str, node_name: str, term: str):
     """Apply bioBERT embeddings to a KG node"
 
     Parameters
     ----------
-    inp_path : str
+    input_path : str
         input data path
-    out_path : str
+    output_path : str
         output data path
     node_name : str
         Name of the KG note that appears in the file name.
-        {inp_path}{node_name}-header.csv
+        {input_path}{node_name}-header.csv
     term : str
         fields in the file corresponding to the node attribute to be encoded
     """
 
-    header = pd.read_csv(f"{inp_path}{node_name}-header.csv", sep="\t")
-    df = pd.read_csv(f"{inp_path}{node_name}-part000.csv", sep="\t", header=None)
+    header = pd.read_csv(f"{input_path}{node_name}-header.csv", sep="\t")
+    df = pd.read_csv(f"{input_path}{node_name}-part000.csv", sep="\t", header=None)
     df.columns = header.columns
 
     if "biobert_emb:double[]" not in df.columns:
@@ -102,7 +111,8 @@ def biobert_enconde_node(inp_path: str, out_path: str, node_name: str, term: str
 
     df["biobert_emb:double[]"] = df["biobert_emb:double[]"].astype(object)
 
-    for i, row in tqdm(df.iterrows(), desc=f"Embedding {node_name}"):
+    print(f"Embedding {node_name}")
+    for i, row in df.iterrows():
 
         sentence = row[term]
 
@@ -111,49 +121,55 @@ def biobert_enconde_node(inp_path: str, out_path: str, node_name: str, term: str
         bio_emb_str = embedding_formatting(bio_emb)
         df.at[i, "biobert_emb:double[]"] = bio_emb_str
 
-    df.to_csv(f"{out_path}{node_name}-part000.csv", sep="\t", header=None, index=False)
+    df.to_csv(f"{output_path}{node_name}-part000.csv", sep="\t", header=None, index=False)
 
 
-def main(inp_path: str, out_path: str):
+def main(args):
 
-    if os.path.exists(out_path):
-        print_red(f"WARNING! {out_path} already exist! in will be overwritten!")
-        shutil.rmtree(out_path)
+    if os.path.exists(args.output_path):
+        print_red(f"WARNING! {args.output_path} already exist! in will be overwritten!")
+        shutil.rmtree(args.output_path)
 
     # Copy the entire input directory into the output directory
-    shutil.copytree(inp_path, out_path)
+    shutil.copytree(args.input_path, args.output_path)
 
-    trial2vect_encode_node(out_path, out_path)
-    biobert_enconde_node(out_path, out_path, "ClinicalTrial", "brief_title")
-    biobert_enconde_node(out_path, out_path, "AdverseEvent", "term")
-    biobert_enconde_node(out_path, out_path, "Biospec", "description")
-    biobert_enconde_node(out_path, out_path, "Condition", "name")
-    biobert_enconde_node(out_path, out_path, "Intervention", "name")
-    biobert_enconde_node(out_path, out_path, "Outcome", "measure")
-    biobert_enconde_node(out_path, out_path, "OrganSystem", "name")
+    trial2vect_encode_node(args.output_path, args.output_path, args)
+    biobert_encode_node(args.output_path, args.output_path, "ClinicalTrial", "brief_title")
+    biobert_encode_node(args.output_path, args.output_path, "AdverseEvent", "term")
+    biobert_encode_node(args.output_path, args.output_path, "Biospec", "description")
+    biobert_encode_node(args.output_path, args.output_path, "Condition", "name")
+    biobert_encode_node(args.output_path, args.output_path, "Intervention", "name")
+    biobert_encode_node(args.output_path, args.output_path, "Outcome", "measure")
+    biobert_encode_node(args.output_path, args.output_path, "OrganSystem", "name")
 
     # update neo4j-admin command
     neo4j_admin = "neo4j-admin-import-call-windows.sh"
 
-    with open(inp_path + neo4j_admin, "r") as f:
+    with open(args.input_path + neo4j_admin, "r") as f:
         command = f.readline()
 
-    windows_inp_path = inp_path.replace("./", "/").replace("/", ("\\"))
-    windows_out_path = out_path.replace("./", "/").replace("/", ("\\"))
-    command = command.replace(windows_inp_path, windows_out_path)
+    windows_input_path = args.input_path.replace("./", "/").replace("/", ("\\"))
+    windows_output_path = args.output_path.replace("./", "/").replace("/", ("\\"))
+    command = command.replace(windows_input_path, windows_output_path)
 
-    with open(out_path + neo4j_admin, "w") as f:
+    with open(args.output_path + neo4j_admin, "w") as f:
         command = f.write(command)
 
-    print_green(f"Knowledge Graph Node Embedding COMPLETED. See {out_path}")
+    print_green(f"Knowledge Graph Node Embedding COMPLETED. See {args.output_path}")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Test llamaindex txt2sql")
+    parser = argparse.ArgumentParser(description="embed clinical trials KG")
     parser.add_argument("input_path", type=str, help="Input path.")
     parser.add_argument("output_path", type=str, help="Output path.")
+    parser.add_argument("-mongoDB", dest="mongoDB", action="store_true", help="Set mongoDB flag to True. If True,  get the CT studies from mongoDB otherwise from ct.gov")
+    parser.add_argument("-user", type=str, help="MongoDB user name")
+    parser.add_argument("-pwd", type=str, help="MongoDB password")
+    parser.add_argument("-app", type=str,default="cluster0", help="MongoDB cluster")
+    parser.add_argument("-db", type=str, default="ctGov", help="MongoDB database name")
+    parser.add_argument("-c", "--collection", type=str, help="MongoDB collection name")
+    parser.set_defaults(mongoDB=False)
+    
     args = parser.parse_args()
-    inp_path = "./data/raw/knowledge_graph/"
-    out_path = "./data/preprocessed/knowledge_graph/"
-    main(args.input_path, args.output_path)
+    main(args)
--- hpc/run_embed_kg_node.sh
+++ hpc/run_embed_kg_node.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -l
 #$ -N ctGov_KG_Embedding
 # Max run time in H:M:S
-#$ -l h_rt=10:0:0
+#$ -l h_rt=12:00:0
 # Memory
-#$ -l mem=1G
-#$ -pe smp 1
-#$ -l gpu=2
+#$ -l mem=2G
+#$ -pe smp 2  # see not askin for smp works with gpus
 
 # workig directory. Use #S -cwd to use current working dir
 #$ -wd /home/rmhijpo/Scratch/ctgov_rag/
-module unload compilers mpi
-module load compilers/gnu/4.9.2
-module load cuda/7.5.18/gnu-4.9.2
+
 module load openssl/1.1.1t python/3.11.3
 module load ruse/2.0
+
+
+set -o allexport
+source .env set
+
+# remove special character (likely added as the file was created on a Windows)
+MONGODB_USER=${MONGODB_USER//$'\r'}
+MONGODB_PWD=${MONGODB_PWD//$'\r'}
+
 
 pip install poetry
 # poetry config virtualenvs.in-project true
 # poetry install
-ruse --stdout --time=900 -s  poetry run python embed_kg_nodes.py ./data/raw/knowledge_graph/ ./data/preprocessed/knowledge_graph/ 
+ruse --stdout --time=900 -s poetry run python embed_kg_nodes.py ./data/raw/knowledge_graph/ ./data/preprocessed/knowledge_graph/ -mongoDB -user $MONGODB_USER -pwd $MONGODB_PWD -db ctGov -c trialgpt
