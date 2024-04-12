759486b1f7e241c9dd7e7e53d172266e9e0b5d7d
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-12 08:25:52 +0100

    reduce timewall closer to the needed time

--- hpc/run_embed_kg_node.sh
+++ hpc/run_embed_kg_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #$ -N ctGov_KG_Embedding
 # Max run time in H:M:S
-#$ -l h_rt=12:00:0
+#$ -l h_rt=7:00:0
 # Memory
 #$ -l mem=2G
 #$ -pe smp 2  # see not askin for smp works with gpus
