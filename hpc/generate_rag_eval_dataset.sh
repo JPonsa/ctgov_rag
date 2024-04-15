e69e24245c9929c63710f63c592855082edc4fea
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-15 18:14:38 +0100

    testing a larger file

--- hpc/generate_rag_eval_dataset.sh
+++ hpc/generate_rag_eval_dataset.sh
@@ -29,11 +29,11 @@ pip install poetry
 ruse --stdout --time=150 -s \
 poetry run python ./src/evaluation/RAGAS.py ./data/RAGA_testset.mistral_7b.csv \
     -user $MONGODB_USER -pwd $MONGODB_PWD -db ctGov -c trialgpt \
-    -n 1 -size 2000 \
+    -n 2 -size 2000 \
     -hf $HF_TOKEN \
     --generator mistralai/Mistral-7B-Instruct-v0.2 \
     --critic mistralai/Mistral-7B-Instruct-v0.2 \
     --embeddings sentence-transformers/all-mpnet-base-v2 \
-    -test_size 5 -s 0.4 -r 0.4 -mc 0.2
+    -test_size 10 -s 0.4 -r 0.4 -mc 0.2
 
 #  --embeddings all-MiniLM-L6-v2 \
