6db1a310ad7ba51aeca9e0ccbb50584740bb5e76
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-18 14:37:04 +0100

    dspy using vLLM

--- hpc/txt2sql_eval.dspy.mistral7b.sh
+++ hpc/txt2sql_eval.dspy.mistral7b.sh
@@ -3,7 +3,7 @@
 # Max run time in H:M:S
 #$ -l h_rt=0:30:0
 # Memory
-#$ -l mem=15G
+#$ -l mem=30G
 #$ -l gpu=1
 
 
@@ -14,8 +14,8 @@ module load openssl/1.1.1t python/3.11.3
 module unload compilers mpi
 module load compilers/gnu/4.9.2
 module load cuda/7.5.18/gnu-4.9.2
-module load ruse/2.0
-module load apptainer
+# module load ruse/2.0
+# module load apptainer
 
 set -o allexport
 source .env set
@@ -27,13 +27,24 @@ HF_TOKEN=${HF_TOKEN//$'\r'}
 
 
 pip install poetry
-ruse --stdout --time=150 -s \
-poetry run python -m vllm.entrypoints.openai.api_server --model mistralai/Mistral-7B-Instruct-v0.2 --port 8000
+
+# Arize tracing
+# ssh -L 6006:myriad.rc.ucl.ac.uk:6006 rmhijpo@ssh-gateway.ucl.ac.uk -f
+# wget https://raw.githubusercontent.com/vllm-project/vllm/main/collect_env.py
+# echo as is
+# python collect_env.py
+# echo from poetry
+# poetry run python collect_env.py
+echo before vllm
+poetry run python -m vllm.entrypoints.openai.api_server --model mistralai/Mistral-7B-Instruct-v0.2 --port 8001 --dtype half &
+echo after vllm
 # apptainer instance start --nv ollama.sif ollama
-ruse --stdout --time=150 -s \
+# ruse --stdout --time=150 -s \
 poetry run python ./src/txt2sql/txt2sql_dspy_test.py -user $AACT_USER -pwd $AACT_PWD \
 -sql_query_template ./src/txt2sql/sql_queries_template.yaml \
 -triplets  ./src/txt2sql/txt2_sql_eval_triplets.tsv \
 -output_dir ./results/txt2sql/ \
--llm llama2 \
--stop '[INST]' '[/INST]', '<<SYS>>', '<</SYS>>'
+-hf $HF_TOKEN \
+-llm mistralai/Mistral-7B-Instruct-v0.2 \
+-stop '[INST]' '[/INST]'
+# -stop '[INST]' '[/INST]', '<<SYS>>', '<</SYS>>'
