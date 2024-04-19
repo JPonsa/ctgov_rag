eba8a2ca91defb6cb79f7fbd1f06746a5ab425ce
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-19 15:08:16 +0100

    llamaindex vllm

--- hpc/txt2sql_eval.llamaindex.mistral7b.sh
+++ hpc/txt2sql_eval.llamaindex.mistral7b.sh
@@ -24,13 +24,18 @@ AACT_USER=${AACT_USER//$'\r'}
 AACT_PWD=${AACT_PWD//$'\r'}
 HF_TOKEN=${HF_TOKEN//$'\r'}
 
+MODE=mistralai/Mistral-7B-Instruct-v0.2
 
 pip install poetry
+poetry run python -m vllm.entrypoints.openai.api_server --model $MODEL --port 8000 --dtype half --enforce-eager &
+echo I am going to sleep
+sleep 5m # Go to sleep so I vLLM server has time to start
+echo I am awake
 ruse --stdout --time=150 -s \
 poetry run python ./src/txt2sql/txt2sql_llamaindex_test.py -user $AACT_USER -pwd $AACT_PWD \
 -sql_query_template ./src/txt2sql/sql_queries_template.yaml \
 -triplets  ./src/txt2sql/txt2_sql_eval_triplets.tsv \
 -output_dir ./results/txt2sql/ \
 -hf $HF_TOKEN \
--llm mistralai/Mistral-7B-Instruct-v0.2 \
+-llm $MODEL \
 -stop '[INST]' '[/INST]'
