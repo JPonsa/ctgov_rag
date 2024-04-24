2303f1441fd84a4ca4c45d95b84a3f19514ac9e3
JPonsa <joan.ponsa.cobas@gmail.com>
2024-04-24 21:10:08 +0100

    set ucl_hp neo4j user

--- src/rag/ReAct.py
+++ src/rag/ReAct.py
@@ -27,9 +27,9 @@ HOST="http://0.0.0.0"
 
 # TODO: Remove credentials
 # Neo4j credentials
-os.environ["NEO4J_URI"] = "bolt://127.0.0.1:7687"
-#os.environ["NEO4J_URI"] = "bolt://0.0.0.0:7687"
-os.environ["NEO4J_USERNAME"] = "neo4j"
+#os.environ["NEO4J_URI"] = "bolt://127.0.0.1:7687"
+os.environ["NEO4J_URI"] = "bolt://0.0.0.0:7687"
+os.environ["NEO4J_USERNAME"] = "ucl_hpc"
 os.environ["NEO4J_PASSWORD"] = "password"
 os.environ["NEO4J_DATABASE"] = "ctgov"
 
