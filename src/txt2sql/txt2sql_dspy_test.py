79f005a4a4422a692e5d77260e5d13b784aab976
JPonsa <joan.ponsa.cobas@gmail.com>
2024-05-06 11:58:43 +0100

    add stop tokens, don't use chat mode

--- src/txt2sql/txt2sql_dspy_test.py
+++ src/txt2sql/txt2sql_dspy_test.py
@@ -434,7 +434,10 @@ def main(args, verbose: bool = False):
         os.environ["HUGGING_FACE_TOKEN"] = args.hf
     
     if args.vllm:
-        lm = dspy.HFClientVLLM(model=args.vllm, port=args.port, url=args.host, max_tokens=1_000, timeout_s=2_000, stop=['\n\n', '<|eot_id|>'], model_type='chat')
+        lm = dspy.HFClientVLLM(model=args.vllm, port=args.port, url=args.host, max_tokens=1_000, timeout_s=2_000, 
+                               stop=['\n\n', '<|eot_id|>'], 
+                            #    model_type='chat',
+                               )
         file_tags.append(args.vllm.split("/")[-1])
         
     elif args.ollama:
