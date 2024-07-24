fdd1ea302db2c8bc0bf6132db99e90b7ae2eda12
JPonsa <joan.ponsa.cobas@gmail.com>
2024-07-24 15:48:05 +0100

    re-order the mode list so analytical and all are last as those are source of failure

--- hpc/ReAct.RAGAS.phi3.sh
+++ hpc/ReAct.RAGAS.phi3.sh
@@ -36,8 +36,7 @@ echo I am going to sleep
 sleep 5m # Go to sleep so I vLLM server has time to start.
 echo I am awake
 
-# for MODE in all sql_only llm_only cypher_only kg_only analytical_only; do
-for MODE in analytical_only kg_only cypher_only llm_only sql_only all; do
+for MODE in llm_only kg_only cypher_only sql_only analytical_only all; do
 
     echo $MODEL_NAME-$MODE
 
