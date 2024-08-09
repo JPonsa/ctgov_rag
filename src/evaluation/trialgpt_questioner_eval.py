47d4676bd7a8361284fba3598de6318ec342d01f
JPonsa <joan.ponsa.cobas@gmail.com>
2024-08-09 15:30:24 +0100

    account for nulls and 0 as denominators

--- src/evaluation/trialgpt_questioner_eval.py
+++ src/evaluation/trialgpt_questioner_eval.py
@@ -1,32 +1,44 @@
 import argparse
+import numpy as np
 import pandas as pd
 
 def precision(references, predictions):
     total = len(predictions)
     numerator = set(predictions).intersection(set(references))
-    return len(numerator) / total
+    return len(numerator) / total or 0
 
 def recall(references, predictions):
     total = len(references)
     numerator = set(references).intersection(set(predictions))
-    return len(numerator) / total
+    return len(numerator) / total or 0
 
 def f1(precision, recall):
+    
+    # Check if precision and recall are both zero
+    if precision == 0 and recall == 0:
+        return 0.0
+    
+    # Check if precision or recall is NaN
+    if np.isnan(precision) or np.isnan(recall):
+        return np.nan
+    
+    # Calculate F1 score
     return 2 * precision * recall / (precision + recall)
-
 
 def main(args):
     
     # read in the data
     df = pd.read_csv(args.input_tsv, sep="\t")
     
+    df.fillna("", inplace=True)
+    
     for i, row in df.iterrows():
         
         if (row[args.y] is None) or (row[args.yhat] is None):
             continue
         
-        references = row[args.y].astype(str).tolist()
-        predictions = row[args.yhat].astype(str).tolist()
+        references = row[args.y].split(",")
+        predictions = row[args.yhat].split(",")
         df.loc[i, "precision"] = precision(references, predictions)
         df.loc[i, "recall"] = recall(references, predictions)
         df.loc[i, "f1"] = f1(df.loc[i, "precision"], df.loc[i, "recall"])
